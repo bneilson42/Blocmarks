4fd83d08e6a3694b1dc39562070c356cb6ddf660
Brad Neilson <bneilson42@ymail.com>
2014-12-21 14:27:05 -0800

    Added likes to bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,18 @@
 class BookmarksController < ApplicationController
   def index
-    @topics = Topic.all
+    @bookmarks = Bookmark.all
   end
 
   def show
-    @topic = Topic.find(params[:id])
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def new
+    @bookmark = Bookmark.new
   end
-end
+
+  private
+    def bookmark_params
+      params.require(:bookmark).permit(:url)
+    end
+ end
